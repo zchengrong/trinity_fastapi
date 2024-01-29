a2f3ef685126baa216a3db2ad4b275fc0bfd7bba
zhouchengrong <Zchengrong@yeah.net>
2024-01-29 11:04:14 +0800

    1

---
+++ gunicorn.py
@@ -0,0 +1,11 @@
+daemon = True  # 是否守护
+bind = '0.0.0.0:9999'  # 绑定
+pidfile = 'gunicorn.pid'  # pid文件地址
+chdir = '.'  # 项目地址
+worker_class = 'uvicorn.workers.UvicornWorker'
+workers = 64
+threads = 64
+loglevel = 'debug'  # 日志级别
+access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
+accesslog = "gunicorn_access.log"
+errorlog = "gunicorn_error.log"
