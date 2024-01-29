4492b9e479459d1f08a8c15800fb55be2e826a1e
zhouchengrong <Zchengrong@yeah.net>
2024-01-29 11:16:00 +0800

    1

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from apps import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=180)
